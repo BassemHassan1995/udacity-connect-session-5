d203135129fb72ca2d9559fae54c05814b89f268
Bassem Hassan <bassem.hassan1995@gmail.com>
2023-02-01 16:40:05 +0200

    use the new Menu Provider API

--- app/src/main/java/bassem/udacity/session5/ui/realestates/RealEstatesFragment.kt
+++ app/src/main/java/bassem/udacity/session5/ui/realestates/RealEstatesFragment.kt
@@ -1,14 +1,20 @@
 package bassem.udacity.session5.ui.realestates
 
 import android.os.Bundle
-import android.view.*
-import android.widget.Toast
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import bassem.udacity.session5.MarsApplication
 import bassem.udacity.session5.R
-import bassem.udacity.session5.databinding.FragmentRealEstatesBinding
 import bassem.udacity.session5.data.model.Type
+import bassem.udacity.session5.databinding.FragmentRealEstatesBinding
 import bassem.udacity.session5.ui.RealEstateViewModelFactory
 import bassem.udacity.session5.ui.RealEstatesViewModel
 
@@ -34,7 +40,7 @@ class RealEstatesFragment : Fragment() {
 
         adapter = RealEstateAdapter()
 
-        setHasOptionsMenu(true)
+        addMenu()
 
         return binding.root
 
@@ -46,7 +52,7 @@ class RealEstatesFragment : Fragment() {
         with(binding) {
             viewModel.allRealEstates.observe(viewLifecycleOwner) {
                 adapter.submitList(it)
-                Toast.makeText(context, getString(R.string.real_estates_found, it.size), Toast.LENGTH_SHORT).show()
+//                Toast.makeText(context, getString(R.string.real_estates_found, it.size), Toast.LENGTH_SHORT).show()
             }
 
             recyclerviewRealEstates.adapter = adapter
@@ -58,21 +64,22 @@ class RealEstatesFragment : Fragment() {
         _binding = null
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.menu_main, menu)
-        super.onCreateOptionsMenu(menu, inflater)
-    }
+    private fun addMenu() {
+        requireActivity().addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) =
+                menuInflater.inflate(R.menu.menu_main, menu)
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.view_rent_menu -> viewModel.setType(Type.RENT)
-            R.id.view_buy_menu -> viewModel.setType(Type.BUY)
-            R.id.view_all_menu -> viewModel.setType(Type.ALL)
-            R.id.clear_menu -> viewModel.deleteAll()
-            R.id.refresh_menu -> viewModel.refreshRealEstates()
-        }
-
-        return true
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                when (menuItem.itemId) {
+                    R.id.view_rent_menu -> viewModel.setType(Type.RENT)
+                    R.id.view_buy_menu -> viewModel.setType(Type.BUY)
+                    R.id.view_all_menu -> viewModel.setType(Type.ALL)
+                    R.id.clear_menu -> viewModel.deleteAll()
+                    R.id.refresh_menu -> viewModel.refreshRealEstates()
+                }
+                return true
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 
 }
